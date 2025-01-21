817e06bb014407123319f733acd7de727b78d613
Liust1918 <705035750@qq.com>
2025-01-21 18:00:34 +0800

    临时

--- C_Studing_Medium/A4_4_mastermind.cpp
+++ C_Studing_Medium/A4_4_mastermind.cpp
@@ -14,6 +14,7 @@
 #include <string.h>
 
 
+
 /*  生成4个不同数字的组合并且存入数组x*/
 void make4digits_1(int x[],int n) {
 	srand((unsigned)time(NULL));
@@ -34,7 +35,7 @@ void make4digits_1(int x[],int n) {
 }
 
 /*检查已输入的字符串s的有效性*/
-int check_1(const char s[],int n) {
+int check_1(const char s[]) {
 	int i, j;
 	int length;
 	//字符串不超过4
@@ -65,11 +66,14 @@ int check_1(const char s[],int n) {
 void judge_1(const char s[], const int n[], int* hit, int* blow,int len) {
 	for (int i=0; i < len; i++) {
 		for (int j = 0; j < len; j++){
-			if (s[i]==n[j]) {
-				blow++;
-			}
-			if (i==j) {
-				hit++;
+			//字符数字和数字进行比较: 数字字符-'0'==数字
+			if (s[i]-'0' == n[j]) {
+				if (i==j) {
+					(*hit)++;
+				}
+				else {
+					(*blow)++;
+				}
 			}
 		}
 	}
@@ -77,20 +81,63 @@ void judge_1(const char s[], const int n[], int* hit, int* blow,int len) {
 
 }
 
-/*显示判断结果*/
+/*显示判断结果
+snum是数字相同
+spos是数字和位置相同
+*/
 void print_result(int snum, int spos) {
+	if (spos ==4) {
+		puts("回答正确");
+	}
+	else if (snum==0) {
+		puts("这些数字里面没有答案数字");
+	}
+	else {
+		printf_s("这些数字里面包括%d个答案数字 \n",snum);
+		if (spos==0) {
+			puts("但是数字的位置都不一致");
+		}
+		else {
+			printf_s("其中有%d个数字的位置是一致的 \n", spos);
+		}
+	}
+
+	
+	//用了n秒
+	//用时 多少秒
+	//请按任意键继续...
 
 }
 
 int main(void) {
 
+	int try_no=0;//输入次数
+	int ckk_code;//已经输入的字符串的检查结果
+	int hit=0;//数字和位置都正确的数字个数
+	int blow=0;//数字正确但是位置不正确的数字个数
+	int n[4];//要猜的数字
+	char result[10];//输入的数字
+	clock_t start, end;//开始时间，结束时间	
 
 	puts("来玩珠玑妙算");
 	puts("猜猜4个数字");
 	puts("其中不包含相同数字");
 	puts("请像4307这样连续输入数字");
 	puts("不能输入空格字符");
-
+	
+	make4digits_1(n, 4);
+	start = clock();
+	do{
+		puts("请输入4个数字");
+		scanf_s("%s", &result);
+		check_1(result);
+		judge_1(result, n, &hit, &blow, 4);
+		print_result(hit + blow, hit); //hit 数字和位置一样  blow 数字一样位置不一样
+		try_no++;
+	} while (hit!=4);
+	end = clock();
+	printf_s("用了%d次,用时%.lf 秒 \n",try_no,(double)(end-start)/CLOCKS_PER_SEC );
+	
 
 	return 0;
 }
